15c29731e69447eb8c5eb079bc173992ab9a5283
KentoTokura <tkento1985@gmail.com>
2023-07-30 01:05:21 +0900

    make up login function

--- sample.sql
+++ sample.sql
@@ -1,4 +1,4 @@
-drop table LoginUser;
-create table LoginUser (username varchar, password varchar, id varchar);
+drop table USERS;
+create table USERS (username varchar, password varchar, id varchar);
 
-insert into LoginUser values ("admin", "admin", "0");
+-- insert into USERS values ("admin", "admin", "0");
