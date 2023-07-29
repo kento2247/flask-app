700cd953590bd5bb47d63caf1a9ce5f8fa8ced06
KentoTokura <tkento1985@gmail.com>
2023-07-29 10:25:50 +0900

    makeup flask demo

--- sample.py
+++ sample.py
@@ -1,1 +1,13 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    hello = "Hello world"
+    return hello
+
+
+if __name__ == "__main__":
+    app.run()
